130d726582199dba0be19a3ff93631824876a37c
ammarazhar <62182371+ammarazhar@users.noreply.github.com>
2020-03-14 22:29:44 +0500

    Updated_file (B)

--- Assignment_2.cpp
+++ Assignment_2.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 int total_column,total_row,start_row,start_column,goal_column, goal_row;
 
+//this is test code for SE GitHub assignment
+
 struct node
 {
 	int rowposition = 0, columnposition = 0;
